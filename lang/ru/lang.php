08d1c7215ec4ca748d53f932668a71baebb16790
Otto Riehl <post@otto-riehl.de>
2021-11-08 13:20:20 +0100

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Otto Riehl <post@otto-riehl.de>
+ */
+$lang['menu']                  = 'Редактор структурных схем';
+$lang['menu_assignments']      = 'Struct Назначения схемы';
+$lang['headline']              = 'Структурированные данные';
+$lang['page schema']           = 'Схема страницы:';
+$lang['lookup schema']         = 'Поиск схемы:';
+$lang['edithl']                = 'Редактирование схемы <i>%s</i>';
+$lang['create']                = 'Создайте новую схему';
+$lang['schemaname']            = 'Название схемы:';
+$lang['save']                  = 'Сохранить';
+$lang['createhint']            = 'Обратите внимание: схемы не могут быть переименованы в дальнейшем';
+$lang['pagelabel']             = 'Страница';
+$lang['rowlabel']              = 'Ряд #';
+$lang['revisionlabel']         = 'Последнее обновление';
+$lang['userlabel']             = 'Последний редактор';
+$lang['summarylabel']          = 'Последнее резюме';
+$lang['summary']               = 'Struct данные структуры';
+$lang['export']                = 'Экспорт схемы в формате JSON';
+$lang['btn_export']            = 'Экспорт';
+$lang['import']                = 'Импорт схемы из JSON';
+$lang['btn_import']            = 'Импорт';
+$lang['import_warning']        = 'ПРЕДУПРЕЖДЕНИЕ: это перезапишет уже определенные поля!';
+$lang['del_confirm']           = 'Введите имя схемы для подтверждения удаления';
+$lang['del_fail']              = 'Имена схем не совпадают. Схема не удалена';
+$lang['del_ok']                = 'Схема была удалена';
+$lang['btn_delete']            = 'Удалить';
+$lang['sort']                  = 'Сортировать по этому столбцу';
+$lang['next']                  = 'Следующая страница';
+$lang['prev']                  = 'Предыдущая страница';
